b1d589875e642561bc294c2f746ab474379f95ea
ajeetwankhede <ajeet@umd.edu>
2018-10-09 22:58:38 -0400

    Created source file containing test stubs for class Astar

---
+++ test/AstarTest.cpp
@@ -0,0 +1,43 @@
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    AstarTest.cpp
+ *  @author  Ajeet Wankhede (driver) and Likhita Madiraju (navigator)
+ *  @date    10/09/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Week 7, Mid-term
+ *
+ *  @section DESCRIPTION
+ *
+ *  Defining the Test stubs for Astar class
+ */
+
+#include <gtest/gtest.h>
+#include <memory>
+#include <Astar.hpp>
+
+std::shared_ptr<Astar> testObject1;
+
+/**
+ * @brief Check if the start node is less than zero
+ */
+TEST(Astar, startNodeTest) {
+  testObject1 = std::make_shared<Astar>();
+  ASSERT_LT(0.0, testObject1->startNode);
+}
+
+/**
+ * @brief Check if the end node is less than zero
+ */
+TEST(Astar, endNodeTest) {
+  testObject1 = std::make_shared<Astar>();
+  ASSERT_LT(0.0, testObject1->endNode);
+}
+
+/**
+ * @brief Check if the resolution is less than zero
+ */
+TEST(Astar, resolutionTest) {
+  testObject1 = std::make_shared<Astar>();
+  ASSERT_LT(0.0, testObject1->resolution);
+}
