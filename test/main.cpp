e3829840c6c6deefecf6b54ea2d0c3ed5594329a
ajeetwankhede <ajeet@umd.edu>
2018-10-09 23:01:04 -0400

    Upated main file with doxygen comments

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,17 @@
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    main.cpp
+ *  @author  Ajeet Wankhede (driver) and Likhita Madiraju (navigator)
+ *  @date    10/09/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Week 7, Mid-term
+ *
+ *  @section DESCRIPTION
+ *
+ *  Main for running all test stubs
+ */
+
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
