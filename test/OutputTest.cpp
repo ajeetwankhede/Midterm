7363c39931c0d2f9e9d58ea7deb3fb1dade81bc6
ajeetwankhede <ajeet@umd.edu>
2018-10-09 22:59:30 -0400

    Created source file containing test stubs for class Output

---
+++ test/OutputTest.cpp
@@ -0,0 +1,19 @@
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    OutputTest.cpp
+ *  @author  Ajeet Wankhede (driver) and Likhita Madiraju (navigator)
+ *  @date    10/09/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Week 7, Mid-term
+ *
+ *  @section DESCRIPTION
+ *
+ *  Defining the Test stubs for Output class
+ */
+
+#include <gtest/gtest.h>
+#include <memory>
+#include <Output.hpp>
+
+std::shared_ptr<Output> testObject4;
