b5dd95c5e46d351a3b2ecbeaea84aae3c79c0cc8
ajeetwankhede <ajeet@umd.edu>
2018-10-09 22:52:28 -0400

    Created source file containing class methods for class Output

---
+++ app/Output.cpp
@@ -0,0 +1,32 @@
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    Output.cpp
+ *  @author  Ajeet Wankhede (driver) and Likhita Madiraju (navigator)
+ *  @date    10/09/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Week 7, Mid-term
+ *
+ *  @section DESCRIPTION
+ *
+ *   Defining methods of Output class
+ */
+
+#include <Output.hpp>
+
+Output::Output() {
+  // Initializing values to the attributes of Output class
+  location = "path";
+}
+
+Output::~Output() {
+  // Destructor stub
+}
+
+void writeTextFile() {
+  // ToDo Write a code to save the trajectory coordinates in a text file
+}
+
+void showOutput() {
+  // ToDo Display the output of A* on a plot
+}
