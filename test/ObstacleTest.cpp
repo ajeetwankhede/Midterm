b2629769083ad0c69957c46549f3505f3417e9d0
ajeetwankhede <ajeet@umd.edu>
2018-10-09 22:59:09 -0400

    Created source file containing test stubs for class Obstacle

---
+++ test/ObstacleTest.cpp
@@ -0,0 +1,19 @@
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    ObstacleTest.cpp
+ *  @author  Ajeet Wankhede (driver) and Likhita Madiraju (navigator)
+ *  @date    10/09/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Week 7, Mid-term
+ *
+ *  @section DESCRIPTION
+ *
+ *  Defining the Test stubs for Obstacle class
+ */
+
+#include <gtest/gtest.h>
+#include <memory>
+#include <Obstacle.hpp>
+
+std::shared_ptr<Obstacle> testObject3;
