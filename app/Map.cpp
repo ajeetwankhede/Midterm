2e8676e5ed99609c3e9d1d172fab71b3db078f85
ajeetwankhede <ajeet@umd.edu>
2018-10-09 22:51:05 -0400

    Created source file containing class methods for class Map

---
+++ app/Map.cpp
@@ -0,0 +1,34 @@
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    Map.cpp
+ *  @author  Ajeet Wankhede (driver) and Likhita Madiraju (navigator)
+ *  @date    10/09/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Week 7, Mid-term
+ *
+ *  @section DESCRIPTION
+ *
+ *  Defining methods of Map class
+ */
+
+#include <Map.hpp>
+
+Map::Map() {
+  // Initializing values to the attributes of Map class
+  length = -0.1;
+  width = -0.1;
+}
+
+Map::~Map() {
+  // Destructor stub
+}
+
+void createMap(double l, double w) {
+  // ToDo Create a plot using gnuplot library of given length and width
+  // ToDo Add obstacles in the plot
+}
+
+void showMap() {
+  // ToDo Display the plot of the map
+}
