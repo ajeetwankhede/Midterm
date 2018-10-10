88c7f343ef20936a56d2aa72be2902c28afd6967
ajeetwankhede <ajeet@umd.edu>
2018-10-09 22:54:26 -0400

    Added to do list in main function

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
-#include <lib.hpp>
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    main.cpp
+ *  @author  Ajeet Wankhede (driver) and Likhita Madiraju (navigator)
+ *  @date    10/09/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Week 7, Mid-term
+ *
+ *  @section DESCRIPTION
+ *
+ *  main for running the path planning module
+ */
 
-int main()
-{
-    dummy();
+#include <iostream>
+
+int main() {
+  // ToDo Add the code for creating a map
+  // ToDo Run the A* algorithm to find the optimal trajectory
     return 0;
 }
