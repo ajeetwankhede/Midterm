e9afbb55d39fd9662125b52bf22c12bd35a49c2f
likhitam <likhitam@umd.edu>
2018-10-16 02:53:46 -0400

    Task: #10 MCL: Added implementation of class Map.

--- app/Map.cpp
+++ app/Map.cpp
@@ -12,37 +12,142 @@
  *  Defining methods of Map class
  */
 
+#include <memory>
 #include <vector>
-#include <Map.hpp>
+#include <map>
 #include <gnuplot-iostream.h>
+#include <Map.hpp>
+#include <Obstacle.hpp>
 
+using std::shared_ptr;
+using std::make_shared;
 using std::vector;
+using std::pair;
+using std::make_pair;
+using std::cout;
+using std::cin;
+using std::endl;
 
 Map::Map() {
   // Initializing values to the attributes of Map class
-  length = -0.1;
-  width = -0.1;
+  length = -1;
+  width = -1;
+  robotL = 0.0;
+  robotW = 0.0;
 }
 
 Map::~Map() {
   // Destructor stub
 }
 
-void Map::createMap(double l, double w) {
+void Map::createMap(int l, int w) {
   length = l;
   width = w;
-  Gnuplot gp;
-  vector<std::pair<double, double> > xy_pts;
-  for (double x = -2; x < 2; x += 0.1) {
-    double y = x * x;
-    xy_pts.push_back(std::make_pair(x, y));
+  bool robotD = false;
+  while (robotD == false) {
+    // Ask to user to enter the robot dimension
+    cout << "Enter the robot length and width: ";
+    cin >> robotL >> robotW;
+    if (robotL >= 0.0 && robotW >= 0.0 && robotL <= width && robotW <= length) {
+      robotD = true;
+    } else {
+      cout << "Wrong robot dimension entered. Please re-enter" << endl;
+    }
   }
-  gp << "set xrange [0:" << l << "]\nset yrange [0:" << w << "]\n";
-  // '-' means read from stdin.  The send1d() function sends data to gnuplot's stdin.
-  gp << "plot'-'with points\n";
-  gp.send1d(xy_pts);
+  int noOb = 0;
+  // Ask the user for number of obstacles in the map
+  cout << "Enter the number of rectangular obstacles in the map: ";
+  cin >> noOb;
+  // Create an object of class Obstacle.
+  vector<Obstacle> O(noOb);
+  vector<pair<double, double> > centerXY;
+  double x = 0.0, y = 0.0, lR = 0.0, wR = 0.0;
+  int i = 0;
+  while (i < noOb) {
+    // Ask user for the center (x,y) of the rectangle
+    cout << "Enter the center x and y coordinate of the rectangle: ";
+    cin >> x >> y;
+    // Form a pair of x,y and add to the vector
+    centerXY.push_back(make_pair(x, y));
+    // Ask the user for the length and breadth of the rectangle
+    cout << "Enter the length and breadth of the rectangle: ";
+    cin >> lR >> wR;
+    // Calculate the vertices from the given data
+    O[i].calculateVertices(x, y, lR, wR);
+    // Verify the obstacle lies within the map boundaries
+    bool verify = O[i].verifyObstacle(length, width);
+    if (verify == true) {
+      // Add the robot dimension to the obstacle
+      O[i].createObstacleSpace(robotL, robotW, length, width);
+      i++;
+    } else {
+      cout << "Wrong dimension entered. Please enter again." << endl;
+    }
+  }
+  i = 0;
+  // Add the obstacle space to xy_pts to plot
+  while (i < noOb) {
+    int j = O[i].x1;
+    while (j <= O[i].x2) {
+      int k = O[i].y1;
+      while (k <= O[i].y3) {
+        xy_pts.push_back(make_pair(j, k));
+        // Create new key-value pairs for obstacle space
+        obstacleSpace[make_pair(j, k)] = 1;
+        k++;
+      }
+      j++;
+    }
+    i++;
+  }
+  i = 0;
+  // Create new key value pairs
+  // map creates new key value pair and initializes to zero if key not found
+  while (i <= width) {
+    int j = 0;
+    while (j <= length) {
+      // Initialize parent cost to zero
+      parentCost[make_pair(i, j)] = 0.0;
+      // Initialize current cost to zero
+      currentCost[make_pair(i, j)] = 0.0;
+      // Initialize visited nodes to zero
+      visitedNodes[make_pair(i, j)] = 0;
+      // Check if key value pair in obstacle already created
+      if (obstacleSpace[make_pair(i, j)] == 1) {
+        // Add obstacle space to visited nodes
+        visitedNodes[make_pair(i, j)] = 1;
+      }
+      j++;
+    }
+    i++;
+  }
 }
 
-void Map::showMap() {
-  // ToDo Display the plot of the map
+void Map::showMap(pair<int, int> startNode, pair<int, int> endNode,
+                  vector<pair<int, int> > path) {
+  Gnuplot gp;
+  // Create a plot of length l(Y-axis) and width w(X-axis)
+  gp << "set xrange [0:" << width << "]\nset yrange [0:" << length << "]\n";
+  //gp << "set style line 1 lc rgb \"red\"\n";
+  // '-' means read from stdin.  The send1d() function sends data to gnuplot's stdin.
+  gp << "set pointsize 1\n";
+  //gp << "set object 1 rect from " << xy_pts[0].first << "," << xy_pts[0].second
+  //   << " to " << xy_pts[3].first << "," << xy_pts[3].second
+  //   << " fc rgb \"red\" fillstyle solid 1.0\n";
+  gp << "plot '-' with points pointtype 7 title 'Obstacle' , "
+     << "'-' with points pointtype 3 title 'Optimal path' , "
+     << "'-' with points pointtype 5 title 'Start and End nodes'"
+     << endl;
+  gp.send1d(xy_pts);
+  gp.send1d(path);
+  vector<pair<int, int> > startEnd;
+  startEnd.push_back(startNode);
+  startEnd.push_back(endNode);
+  gp.send1d(startEnd);
+}
+
+void Map::changeRobotDimension(double length, double width) {
+  // Update the robot length and width
+  robotL = length;
+  robotW = width;
 }
