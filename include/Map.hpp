1acb74695c5ce06f994a57560630821869014b49
likhitam <likhitam@umd.edu>
2018-10-16 02:58:14 -0400

    Task #10 MCL: Updated Map header file with class implementation.

--- include/Map.hpp
+++ include/Map.hpp
@@ -12,8 +12,11 @@
  *  Map class declaration
  */
 
-#ifndef CPP_BOILERPLATE_INCLUDE_MAP_HPP_
-#define CPP_BOILERPLATE_INCLUDE_MAP_HPP_
+#ifndef INCLUDE_MAP_HPP_
+#define INCLUDE_MAP_HPP_
+
+#include <map>
+#include <vector>
 
 /**
  * @brief Class declaration for Map
@@ -33,24 +36,44 @@ class Map {
   /**
    *   @brief Call this method to create a new Map
    *
-   *   @param double value of length of the map
-   *   @param double value of width of the map
+   *   @param int value of length of the map
+   *   @param int value of width of the map
    *
    *   @return none
    */
-  void createMap(double l, double w);
+  void createMap(int l, int w);
 
   /**
    *   @brief Call this method to modify the Map
    *
-   *   @param none
+   *   @param pair of int value of (x,y) coordinates of start node
+   *   @param pair of int value of (x,y) coordinates of end node
+   *   @param vector of pair of int value of (x,y) coordinates of path
    *
    *   @return none
    */
 
-  void showMap();
+  void showMap(std::pair<int, int> startNode, std::pair<int, int> endNode,
+               std::vector<std::pair<int, int> > path);
 
-  double length, width;
+  /**
+   *   @brief Change the dimension of the robot
+   *
+   *   @param double value of length of the robot
+   *   @param double value of width of the robot
+   *
+   *   @return none
+   */
+  void changeRobotDimension(double length, double width);
+
+  int length, width;
+  double robotL, robotW;
+  std::vector<std::pair<int, int> > xy_pts;
+  std::map<std::pair<int, int>, int> obstacleSpace;
+  std::map<std::pair<int, int>, int> visitedNodes;
+  std::map<std::pair<int, int>, double> parentCost;
+  std::map<std::pair<int, int>, double> currentCost;
+  std::map<std::pair<int, int>, std::pair<int, int> > parentNode;
 };
 
-#endif  // CPP_BOILERPLATE_INCLUDE_MAP_HPP_
+#endif  // INCLUDE_MAP_HPP_
