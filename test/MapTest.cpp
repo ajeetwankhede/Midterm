82fd644c7b4e51220b613c488b55d9a8f1622db9
likhitam <likhitam@umd.edu>
2018-10-16 22:36:32 -0400

    Task #6 TST: Subtask #2 MTST: Added tests for class map.

--- test/MapTest.cpp
+++ test/MapTest.cpp
@@ -12,11 +12,40 @@
  *  Defining the Test stubs for Map class
  */
 
+#include "gmock/gmock.h"
 #include <gtest/gtest.h>
 #include <memory>
 #include <Map.hpp>
 
+using ::testing::Return;
+using ::testing::AtLeast;
+
 std::shared_ptr<Map> testObject2;
+
+class Obstacle {
+  virtual ~Obstacle();
+  virtual bool verifyObstacle(const int length, const int width) = 0;
+  virtual void createObstacleSpace(double robotL, double robotW, int length,
+                                   int width) = 0;
+  virtual void calculateVertices(double centerX, double centerY, double length,
+                                 double width) = 0;
+};
+
+class MockObstacle : public Obstacle {
+  MOCK_METHOD2(verifyObstacle, bool(const int length, const int width));
+  MOCK_METHOD4(createObstacleSpace, void(double robotL, double robotW, int length,
+          int width));
+  MOCK_METHOD4(calculateVertices, void(double centerX, double centerY, double length,
+          double width));
+};
+
+TEST(Map, createMapTest) {
+  MockObstacle O;
+  //EXPECT_CALL(O, verifyObstacle(1,1)).WillOnce(Return(true));
+  EXPECT_CALL(O, calculateVertices(5.0,5.0,1.0,1.0)).Times(AtLeast(2));
+  testObject2 = std::make_shared<Map>();
+  EXPECT_TRUE(testObject2->createMap(10, 10, 'Y'));
+}
 
 /**
  * @brief Check if the length of map is less than zero
@@ -32,4 +61,22 @@ TEST(Map, lengthTest) {
 TEST(Map, widthTest) {
   testObject2 = std::make_shared<Map>();
   ASSERT_LE(0, testObject2->width);
+}
+
+/**
+ * @brief Verify the changeRobotDimension of the map
+ */
+TEST(Map, changeRobotDimensionTest1) {
+  testObject2 = std::make_shared<Map>();
+  testObject2->changeRobotDimension(1.0, 1.0);
+  ASSERT_EQ(1.0, testObject2->robotL);
+}
+
+/**
+ * @brief Verify the changeRobotDimension of the map
+ */
+TEST(Map, changeRobotDimensionTest2) {
+  testObject2 = std::make_shared<Map>();
+  testObject2->changeRobotDimension(1.0, 1.0);
+  ASSERT_EQ(1.0, testObject2->robotW);
 }
