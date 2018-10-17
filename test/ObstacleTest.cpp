47c2c849a565c141bfa2aa1bcc50233f615159e9
likhitam <likhitam@umd.edu>
2018-10-16 22:37:32 -0400

    Task #6 TST: Subtask #3 OBTST: Added tests for class Obstacle.

--- test/ObstacleTest.cpp
+++ test/ObstacleTest.cpp
@@ -17,3 +17,48 @@
 #include <Obstacle.hpp>
 
 std::shared_ptr<Obstacle> testObject3;
+
+/**
+ * @brief Check if the calculateVertices works
+ */
+TEST(Obstacle, calculateVerticesTest1) {
+  testObject3 = std::make_shared<Obstacle>();
+  testObject3->calculateVertices(5.0, 5.0, 1.0, 1.0);
+  ASSERT_EQ(4, testObject3->x1);
+}
+
+/**
+ * @brief Check if the calculateVertices works
+ */
+TEST(Obstacle, calculateVerticesTest2) {
+testObject3 = std::make_shared<Obstacle>();
+  testObject3->calculateVertices(5.0, 5.0, 1.0, 1.0);
+  ASSERT_EQ(5, testObject3->x2);
+}
+
+/**
+ * @brief Check if the calculateVertices works
+ */
+TEST(Obstacle, calculateVerticesTest3) {
+  testObject3 = std::make_shared<Obstacle>();
+  testObject3->calculateVertices(5.0, 5.0, 1.0, 1.0);
+  ASSERT_EQ(4, testObject3->y1);
+}
+
+/**
+ * @brief Check if the calculateVertices works
+ */
+TEST(Obstacle, calculateVerticesTest4) {
+  testObject3 = std::make_shared<Obstacle>();
+  testObject3->calculateVertices(5.0, 5.0, 1.0, 1.0);
+  ASSERT_EQ(5, testObject3->y3);
+}
+
+/**
+ * @brief Check if the createObstacleSpace works
+ */
+TEST(Obstacle, verifyObstacleTest) {
+  testObject3 = std::make_shared<Obstacle>();
+  testObject3->calculateVertices(5.0, 5.0, 1.0, 1.0);
+  ASSERT_EQ(1, testObject3->verifyObstacle(10, 10));
+}
