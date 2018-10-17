ed28d9d4ae47d54476fe28ba485e9d282a4895cc
likhitam <likhitam@umd.edu>
2018-10-16 23:32:47 -0400

    Task #16 ATST: Subtask #1 AAT Added tests for Astar class.

--- test/AstarTest.cpp
+++ test/AstarTest.cpp
@@ -13,10 +13,20 @@
  */
 
 #include <gtest/gtest.h>
+#include <utility>
 #include <memory>
 #include <Astar.hpp>
 
 std::shared_ptr<Astar> testObject1;
+using std::make_pair;
+
+/**
+ * @brief Verify runAstar method
+ */
+TEST(Astar, runAstarTest) {
+  testObject1 = std::make_shared<Astar>();
+  ASSERT_LE(0, testObject1->startNode.first);
+}
 
 /**
  * @brief Check if the start node is less than zero
@@ -32,4 +42,42 @@ TEST(Astar, startNodeTest) {
 TEST(Astar, endNodeTest) {
   testObject1 = std::make_shared<Astar>();
   ASSERT_LE(0, testObject1->endNode.first);
+}
+
+/**
+ * @brief Check if verifyNodes work
+ */
+TEST(Astar, verifyNodesTest1) {
+  testObject1 = std::make_shared<Astar>();
+  testObject1->startNode = std::make_pair(1, 1);
+  testObject1->endNode = std::make_pair(9, 9);
+  testObject1->length = 10;
+  testObject1->width = 10;
+  ASSERT_EQ(1, testObject1->verifyNodes());
+}
+
+/**
+ * @brief Check if verifyNodes work
+ */
+TEST(Astar, verifyNodesTest2) {
+  testObject1 = std::make_shared<Astar>();
+  testObject1->startNode = std::make_pair(1, 1);
+  testObject1->endNode = std::make_pair(9, 9);
+  testObject1->length = 10;
+  testObject1->width = 10;
+  testObject1->obstacleSpace[testObject1->startNode] = 1;
+  ASSERT_EQ(0, testObject1->verifyNodes());
+}
+
+/**
+ * @brief Check if verifyNodes work
+ */
+TEST(Astar, verifyNodesTest3) {
+  testObject1 = std::make_shared<Astar>();
+  testObject1->startNode = std::make_pair(1, 1);
+  testObject1->endNode = std::make_pair(9, 9);
+  testObject1->length = 10;
+  testObject1->width = 10;
+  testObject1->obstacleSpace[testObject1->endNode] = 1;
+  ASSERT_EQ(0, testObject1->verifyNodes());
 }
