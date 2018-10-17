2d41d85b948b3b5451b1465902ab609ed86b20d5
likhitam <likhitam@umd.edu>
2018-10-16 23:32:18 -0400

    Task #16 ATST: Subtask #1 MAT Added tests for Map class.

--- app/Map.cpp
+++ app/Map.cpp
@@ -14,14 +14,13 @@
 
 #include <gnuplot-iostream.h>
 #include <vector>
-#include <memory>
+#include <string>
 #include <map>
 #include <Map.hpp>
 #include <Obstacle.hpp>
 
-using std::shared_ptr;
-using std::make_shared;
 using std::vector;
+using std::string;
 using std::pair;
 using std::make_pair;
 using std::cout;
@@ -40,86 +39,143 @@ Map::~Map() {
   // Destructor stub
 }
 
-void Map::createMap(int l, int w) {
-  length = l;
-  width = w;
-  bool robotD = false;
-  while (robotD == false) {
-    // Ask to user to enter the robot dimension
-    cout << "Enter the robot length and width: ";
-    cin >> robotL >> robotW;
-    if (robotL >= 0.0 && robotW >= 0.0 && robotL <= width && robotW <= length) {
-      robotD = true;
-    } else {
-      cout << "Wrong robot dimension entered. Please re-enter" << endl;
-    }
-  }
-  int noOb = 0;
-  // Ask the user for number of obstacles in the map
-  cout << "Enter the number of rectangular obstacles in the map: ";
-  cin >> noOb;
-  // Create an object of class Obstacle.
-  vector<Obstacle> O(noOb);
-  vector<pair<double, double> > centerXY;
-  double x = 0.0, y = 0.0, lR = 0.0, wR = 0.0;
-  int i = 0;
-  while (i < noOb) {
-    // Ask user for the center (x,y) of the rectangle
-    cout << "Enter the x and y coordinate of center the rectangle: ";
-    cin >> x >> y;
-    // Form a pair of x,y and add to the vector
-    centerXY.push_back(make_pair(x, y));
-    // Ask the user for the length and breadth of the rectangle
-    cout << "Enter the length and breadth of the rectangle: ";
-    cin >> lR >> wR;
+void Map::createMap(int l, int w, char demo) {
+  if (demo == 'Y') {
+    // Initializing values to the attributes of Map class
+    robotL = 1.0;
+    robotW = 1.0;
+    length = 100;
+    width = 100;
+    int noOb = 2;
+    // Create an object of class Obstacle.
+    vector<Obstacle> O(noOb);
     // Calculate the vertices from the given data
-    O[i].calculateVertices(x, y, lR, wR);
-    // Verify the obstacle lies within the map boundaries
-    bool verify = O[i].verifyObstacle(length, width);
-    if (verify == true) {
-      // Add the robot dimension to the obstacle
-      O[i].createObstacleSpace(robotL, robotW, length, width);
+    O[0].calculateVertices(30.0, 40.0, 10.0, 10.0);
+    // Add the robot dimension to the obstacle
+    O[0].createObstacleSpace(robotL, robotW, length, width);
+    // Calculate the vertices from the given data
+    O[1].calculateVertices(70.0, 70.0, 10.0, 10.0);
+    // Add the robot dimension to the obstacle
+    O[1].createObstacleSpace(robotL, robotW, length, width);
+    int i = 0;
+    // Add the obstacle space to xy_pts to plot
+    while (i < noOb) {
+      int j = O[i].x1;
+      while (j <= O[i].x2) {
+        int k = O[i].y1;
+        while (k <= O[i].y3) {
+          xy_pts.push_back(make_pair(j, k));
+          // Create new key-value pairs for obstacle space
+          obstacleSpace[make_pair(j, k)] = 1;
+          k++;
+        }
+        j++;
+      }
       i++;
-    } else {
-      cout << "Wrong dimension entered. Please enter again." << endl;
     }
-  }
-  i = 0;
-  // Add the obstacle space to xy_pts to plot
-  while (i < noOb) {
-    int j = O[i].x1;
-    while (j <= O[i].x2) {
-      int k = O[i].y1;
-      while (k <= O[i].y3) {
-        xy_pts.push_back(make_pair(j, k));
-        // Create new key-value pairs for obstacle space
-        obstacleSpace[make_pair(j, k)] = 1;
-        k++;
+    i = 0;
+    // Create new key value pairs
+    // map creates new key value pair and initializes to zero if key not found
+    while (i <= width) {
+      int j = 0;
+      while (j <= length) {
+        // Initialize parent cost to zero
+        parentCost[make_pair(i, j)] = 0.0;
+        // Initialize current cost to zero
+        currentCost[make_pair(i, j)] = 0.0;
+        // Initialize visited nodes to zero
+        visitedNodes[make_pair(i, j)] = 0;
+        // Check if key value pair in obstacle already created
+        if (obstacleSpace[make_pair(i, j)] == 1) {
+          // Add obstacle space to visited nodes
+          visitedNodes[make_pair(i, j)] = 1;
+        }
+        j++;
       }
-      j++;
+      i++;
     }
-    i++;
-  }
-  i = 0;
-  // Create new key value pairs
-  // map creates new key value pair and initializes to zero if key not found
-  while (i <= width) {
-    int j = 0;
-    while (j <= length) {
-      // Initialize parent cost to zero
-      parentCost[make_pair(i, j)] = 0.0;
-      // Initialize current cost to zero
-      currentCost[make_pair(i, j)] = 0.0;
-      // Initialize visited nodes to zero
-      visitedNodes[make_pair(i, j)] = 0;
-      // Check if key value pair in obstacle already created
-      if (obstacleSpace[make_pair(i, j)] == 1) {
-        // Add obstacle space to visited nodes
-        visitedNodes[make_pair(i, j)] = 1;
+  } else {
+    length = l;
+    width = w;
+    bool robotD = false;
+    while (robotD == false) {
+      // Ask to user to enter the robot dimension
+      cout << "Enter the robot length and width: ";
+      cin >> robotL >> robotW;
+      if (robotL >= 0.0 && robotW >= 0.0 && robotL <= width
+          && robotW <= length) {
+        robotD = true;
+      } else {
+        cout << "Wrong robot dimension entered. Please re-enter" << endl;
       }
-      j++;
     }
-    i++;
+    int noOb = 0;
+    // Ask the user for number of obstacles in the map
+    cout << "Enter the number of rectangular obstacles in the map: ";
+    cin >> noOb;
+    // Create an object of class Obstacle.
+    vector<Obstacle> O(noOb);
+    vector<pair<double, double> > centerXY;
+    double x = 0.0, y = 0.0, lR = 0.0, wR = 0.0;
+    int i = 0;
+    while (i < noOb) {
+      // Ask user for the center (x,y) of the rectangle
+      cout << "Enter the x and y coordinate of center the rectangle: ";
+      cin >> x >> y;
+      // Form a pair of x,y and add to the vector
+      centerXY.push_back(make_pair(x, y));
+      // Ask the user for the length and breadth of the rectangle
+      cout << "Enter the length and breadth of the rectangle: ";
+      cin >> lR >> wR;
+      // Calculate the vertices from the given data
+      O[i].calculateVertices(x, y, lR, wR);
+      // Verify the obstacle lies within the map boundaries
+      bool verify = O[i].verifyObstacle(length, width);
+      if (verify == true) {
+        // Add the robot dimension to the obstacle
+        O[i].createObstacleSpace(robotL, robotW, length, width);
+        i++;
+      } else {
+        cout << "Wrong dimension entered. Please enter again." << endl;
+      }
+    }
+    i = 0;
+    // Add the obstacle space to xy_pts to plot
+    while (i < noOb) {
+      int j = O[i].x1;
+      while (j <= O[i].x2) {
+        int k = O[i].y1;
+        while (k <= O[i].y3) {
+          xy_pts.push_back(make_pair(j, k));
+          // Create new key-value pairs for obstacle space
+          obstacleSpace[make_pair(j, k)] = 1;
+          k++;
+        }
+        j++;
+      }
+      i++;
+    }
+    i = 0;
+    // Create new key value pairs
+    // map creates new key value pair and initializes to zero if key not found
+    while (i <= width) {
+      int j = 0;
+      while (j <= length) {
+        // Initialize parent cost to zero
+        parentCost[make_pair(i, j)] = 0.0;
+        // Initialize current cost to zero
+        currentCost[make_pair(i, j)] = 0.0;
+        // Initialize visited nodes to zero
+        visitedNodes[make_pair(i, j)] = 0;
+        // Check if key value pair in obstacle already created
+        if (obstacleSpace[make_pair(i, j)] == 1) {
+          // Add obstacle space to visited nodes
+          visitedNodes[make_pair(i, j)] = 1;
+        }
+        j++;
+      }
+      i++;
+    }
   }
 }
 
